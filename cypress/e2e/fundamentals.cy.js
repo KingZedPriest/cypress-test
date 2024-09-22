fa869682905aacbdc99252ee718d1185a23c02c7
Priest <Charleschukwuemeka47@gmail.com>
2024-09-22 11:34:41 +0100

    Feat: More advance tests

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,12 +1,24 @@
-describe('Fundamental test', () => {
-  it('Contains correct header text', () => {
-    cy.visit('/fundamentals');
-    cy.get('[data-test="fundamentals-header"]').should("contain.text", "Testing Fundamentals");
+describe("Fundamental test", () => {
+  beforeEach(() => {
+    cy.visit("/fundamentals");
   });
-  it('Accordion works correctly', () => {
-    cy.visit('/fundamentals');
-    cy.contains(/Your tests will exist in a describe block/i).should("not.be.visible");
-    cy.get('[data-test="accordion-item-1"]').click()
-    cy.contains(/Your tests will exist in a describe block/i).should("be.visible");
+  it("Contains correct header text", () => {
+    cy.get('[data-test="fundamentals-header"]').should(
+      "contain.text",
+      "Testing Fundamentals"
+    );
   });
-}); 
+  it("Accordion works correctly", () => {
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
