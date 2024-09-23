5bce144bc95a0dcc681a08c360c93523bb5c5fab
Priest <Charleschukwuemeka47@gmail.com>
2024-09-23 10:34:56 +0100

    Feat: Add aliases and use some cypress wait

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,12 @@ describe("Form test", () => {
     });
     it("Test subscribe form", () => {
         cy.contains(/testing forms/i);
-        cy.getDataTest("subscribe-form").find("input").type("sample@email.com");
-        cy.contains(/Successfully subbed: sample@email.com!/i).should("not.exist")
+        cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
+        cy.get("@subscribe-input").type("sample@email.com");
+        cy.contains(/Successfully subbed: sample@email.com!/i).should("not.exist");
         cy.getDataTest("subscribe-button").click();
-        cy.contains(/Successfully subbed: sample@email.com!/i).should("exist")
+        cy.contains(/Successfully subbed: sample@email.com!/i).should("exist");
+        cy.wait(3000);
+        cy.contains(/Successfully subbed: sample@email.com!/i).should("not.exist");
     });
 })
