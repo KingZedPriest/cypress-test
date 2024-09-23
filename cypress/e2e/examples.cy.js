95e978bbe5e668ce281de6ae2fb1c38e547459c6
Priest <Charleschukwuemeka47@gmail.com>
2024-09-23 22:17:02 +0100

    Feat: Add the Pages Test

---
+++ cypress/e2e/examples.cy.js
@@ -0,0 +1,33 @@
+describe('Various examples', () => {
+    beforeEach(() => {
+        cy.visit("/examples")
+    })
+    it('multi-page testing', () => {
+        cy.getDataTest("nav-why-cypress").click();
+        cy.location("pathname").should("equal", "/")
+
+        //OVerview Page
+        cy.getDataTest("nav-overview").click();
+        cy.location("pathname").should("equal", "/overview")
+
+        //Fundamentals Page
+        cy.getDataTest("nav-fundamentals").click();
+        cy.location("pathname").should("equal", "/fundamentals")
+
+        //Forms Page
+        cy.getDataTest("nav-forms").click();
+        cy.location("pathname").should("equal", "/forms")
+
+        //Examples Page
+        cy.getDataTest("nav-examples").click();
+        cy.location("pathname").should("equal", "/examples")
+
+        //Component Page
+        cy.getDataTest("nav-component").click();
+        cy.location("pathname").should("equal", "/component")
+
+        //Best Practices Page
+        cy.getDataTest("nav-best-practices").click();
+        cy.location("pathname").should("equal", "/best-practices")
+    })
+})
